ed13ebd7ab4821953b10056f5e2da1a04dd3a532
Dmytro Zhuravskyi <dimawrmail@gmail.com>
2026-01-06 14:47:14 +0200

    optim

---
+++ frontend/js/fullscreen_util.js
@@ -0,0 +1,11 @@
+function toggleAppFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen().catch(err => {
+            console.log(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}
